ea095bdb15a716804efde67015d88c04dd32fec2
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-10 00:04:29 -0800

    ADV Task 2 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
