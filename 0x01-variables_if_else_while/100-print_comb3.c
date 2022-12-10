a9d95d387862da139355445cc13bedb2ae252242
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-10 00:36:08 -0800

    ADV Task 1 - 100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,19 +16,19 @@ int main(void)
 
 	for (tens = '0'; tens <= '9'; tens++)
 	{
-	    for (ones = '0'; ones <= '9'; ones++)
-	    {
-		 if (!((ones == tens) || (tens > ones)))
-		 {
-		    putchar(tens);
-		    putchar(ones);
-		    if (!(ones == '9' && tens == '8'))
-		    {
-		       putchar(',');
-		       putchar(' ');
-		    }
-		 }
-	    }
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+		    		putchar(ones);
+		    		if (!(ones == '9' && tens == '8'))
+		    		{
+					putchar(',');
+		       			putchar(' ');
+		    		}
+		 	}
+	    	}
 	}
 
 	putchar('\n');
