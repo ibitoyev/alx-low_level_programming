95208860da67f2312c1ccf5259888073e4f96bbd
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-13 15:52:22 -0800

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Prints _putchar and a new line
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  *
  */
 
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
