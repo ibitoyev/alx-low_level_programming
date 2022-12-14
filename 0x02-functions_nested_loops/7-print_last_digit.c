528368276a871ec815220ff3fa3456ea76a6200e
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-14 08:50:24 -0800

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,24 @@
 /**
  * print_last_digit - prints the last digits of a number
  *
- * @n: the number in question.
+ * @n: input number as an integer
  * Return: Value of the last digits.
  */
 
 int print_last_digit(int n)
 
 {
-	int last_digit = n % 10;
+	int 1;
 
-	if (last_digit < 0)
-		last_digit *= -1;
-		_putchar (last_digit + '0');
-		return (last_digit);
+	1 = n % 10;
+	if (1 < 0)
+	{
+		_putchar (-1 + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (1);
+	}
 }
