aba287d9014c33d610d4a4eaedc293137e02c03e
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-14 14:07:22 -0800

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Test function for positive  or negative
+ * main - Tests function that prints if integer is positive  or negative
  *
  * Return: 0
  */
 
 int main(void)
-
 {
 	int i;
 
