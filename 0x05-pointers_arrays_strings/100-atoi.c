99942d1db813c0efa434a38ecd7984abc9c46914
Victor Ibitoye <ibitoyevictor1@gmail.com>
2022-12-19 02:54:21 -0800

    Task 100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, j = 0, k = 1, m = 1, i;
 
-	while (*(s + count) !+ '\0')
+	while (*(s + count) != '\0')
 	{
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
 		{
 			if (size > 0)
-				m *= 10
+				m *= 10;
 			size++;
 		}
 		count++;
@@ -29,7 +29,7 @@ int _atoi(char *s)
 
 	for (i = count - size; i < count; i++)
 	{
-		j = j + ((*(s + i) - 48) * m)
+		j = j + ((*(s + i) - 48) * m);
 		m /= 10;
 	}
 	return (j * k);
