d56a048df5d3beb9a0887bc2febe4fca0045862d
Victor Ibitoye <ibitoyevictor1@gmail.com>
2023-01-06 14:03:43 -0800

    Task 0

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; *(s + 1) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + 1) == *(accept + j))
+				return (s + 1);
+		}
+	}
+
+	return ('\0');
+}
