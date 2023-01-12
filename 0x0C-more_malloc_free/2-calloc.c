e0a65278ae22463f33999fbe348a770d3e76e982
Victor Ibitoye <ibitoyevictor1@gmail.com>
2023-01-12 06:05:17 -0800

    Task 2.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
 
 /**
  * _calloc - allocates memory for an array using malloc
@@ -9,8 +31,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	unsigned int i;
+	void *p;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -22,11 +43,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	
-	for (i = 0; i < nmemb * size; i++)
-	{
-		p[i] = 0;
-	}
-	
+	_memset(p, 0, (nmemb * size));
+
 	return (p);
 }
